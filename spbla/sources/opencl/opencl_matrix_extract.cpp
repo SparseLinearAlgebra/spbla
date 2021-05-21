a1d8c4a5af25375a9b5efa5e5039b77e5586c399
Maria Karpenko <mkarpenko.spb@mail.ru>
2021-05-21 22:46:37 +0300

    [CODE] submatrix passed

--- spbla/sources/opencl/opencl_matrix_extract.cpp
+++ spbla/sources/opencl/opencl_matrix_extract.cpp
@@ -31,6 +31,9 @@
 namespace spbla {
 
     void OpenCLMatrix::extract(index *rows, index *cols, size_t &nvals) {
+        if (nvals == 0) {
+            return;
+        }
         cl::Event evRow, evCol;
         clbool::matrix_coo mCoo = clbool::dcsr_to_coo_shallow(*clboolState, mMatrixImpl);
 
@@ -39,8 +42,6 @@ namespace spbla {
         clboolState->queue.enqueueReadBuffer(mCoo.cols_gpu(), false, 0, sizeof(index) * nvals,
                                               cols, nullptr, &evCol);
         evRow.wait(); evCol.wait();
-
-        updateFromImpl();
     }
 
 }
